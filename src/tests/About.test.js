65d63e5b17acd35856ce343a027d1890d2ce41eb
Alex <alexsandrolima991@hotmail.com>
2022-08-16 17:05:44 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { About } from '../pages';
+
+const srcExpected = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+describe('Testando a About.js', () => {
+  test('Testa se exite um Heading level 2, com o texto "About Pokédex" na rota "/about"',
+    () => {
+      render(<About />);
+
+      const heading = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+      expect(heading).toBeInTheDocument();
+    });
+
+  test('Testa se a imagem tem o mesmo src do "srcExpected"',
+    () => {
+      render(<About />);
+
+      const image = screen.getByRole('img');
+
+      expect(image).toHaveAttribute('src', srcExpected);
+      expect(image).toHaveAttribute('alt', 'Pokédex');
+    });
+});
