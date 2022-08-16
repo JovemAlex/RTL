68245c85ab0e2c0ab57cb59dc1d4146b55741192
Alex <alexsandrolima991@hotmail.com>
2022-08-16 17:27:45 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+
+describe('Testando a FavoritePokemons.js', () => {
+  test('Testa se o texto "No favorite pokemon found" aparece', () => {
+    render(<FavoritePokemons />);
+    const text = screen.getByText('No favorite pokemon found');
+    expect(text).toBeInTheDocument();
+  });
+});
